5ec42afb89ce513e6ae23fdee1de58103609cca0
Sankalp-CS <sankalpbaraskar@gmail.com>
2026-08-02 23:52:02 +0530

    Time: 14 ms (36.35%), Space: 46.4 MB (12.24%) - LeetHub

---
+++ 1006-clumsy-factorial/1006-clumsy-factorial.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int clumsy(int n) {
+         Stack<Integer> stack = new Stack<>();
+        stack.push(n);
+
+        int op = 0; // 0=*, 1=/, 2=+, 3=-
+
+        for (int i = n - 1; i >= 1; i--) {
+
+            if (op == 0) {
+                stack.push(stack.pop() * i);
+            } 
+            else if (op == 1) {
+                stack.push(stack.pop() / i);
+            } 
+            else if (op == 2) {
+                stack.push(i);
+            } 
+            else {
+                stack.push(-i);
+            }
+
+            op = (op + 1) % 4;
+        }
+
+        int ans = 0;
+        while (!stack.isEmpty()) {
+            ans += stack.pop();
+        }
+
+        return ans;
+    }
+}
