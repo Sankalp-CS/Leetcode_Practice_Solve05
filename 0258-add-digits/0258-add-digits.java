a79f693c8a33008a4d18ac5e56c16e4e27f905eb
Sankalp-CS <sankalpbaraskar@gmail.com>
2026-08-24 09:20:36 +0530

    Time: 1 ms (98.04%), Space: 42.8 MB (9.72%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int addDigits(int num) {
+        int sum=0;
+        while(num>0){
+            int digit=num%10;
+            sum+=digit;
+            num/=10;
+        }
+        if(sum>9){
+            return addDigits(sum);
+        }
+        return sum;
+    }
+}
