7242ac9dd14974abdd41f6652baf67947c0a4e5e
Sankalp-CS <sankalpbaraskar@gmail.com>
2026-09-07 21:39:25 +0530

    Time: 2 ms (97.25%), Space: 44 MB (74.66%) - LeetHub

--- 3411-maximum-subarray-with-equal-products/3411-maximum-subarray-with-equal-products.java
+++ 3411-maximum-subarray-with-equal-products/3411-maximum-subarray-with-equal-products.java
@@ -1,34 +1,22 @@
-import java.math.BigInteger;
-
 class Solution {
-
     public int maxLength(int[] nums) {
-        int n = nums.length;
-        int ans = 1;
-
+        int n = nums.length, res = 0;
         for (int i = 0; i < n; i++) {
-
-            BigInteger product = BigInteger.ONE;
-            BigInteger lcm = BigInteger.ONE;
-            BigInteger gcd = BigInteger.ZERO;
-
+            long prod = 1, g = 0, l = 1;
             for (int j = i; j < n; j++) {
-
-                BigInteger x = BigInteger.valueOf(nums[j]);
-
-                product = product.multiply(x);
-
-                gcd = gcd.gcd(x);
-
-                BigInteger common = lcm.gcd(x);
-                lcm = lcm.divide(common).multiply(x);
-
-                if (product.equals(gcd.multiply(lcm))) {
-                    ans = Math.max(ans, j - i + 1);
-                }
+                prod *= nums[j];
+                g = gcd(g, nums[j]);
+                l = lcm(l, nums[j]);
+                if (prod == g * l) res = Math.max(res, j - i + 1);
+                else if (prod > g * l) break;
             }
         }
-
-        return ans;
+        return res;
+    }
+    private long gcd(long a, long b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+    private long lcm(long a, long b) {
+        return a / gcd(a, b) * b;
     }
 }
