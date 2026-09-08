5425edde2135c63b0edb6758a3e748fb63f1bd7a
Sankalp-CS <sankalpbaraskar@gmail.com>
2026-09-08 09:17:04 +0530

    Time: 1 ms (83.65%), Space: 42.4 MB (72.85%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int countCommas(int n) {
+        String s=String.valueOf(n);
+        if(s.length()<4){
+            return 0;
+        }
+        return n-1000+1;
+    }
+}
